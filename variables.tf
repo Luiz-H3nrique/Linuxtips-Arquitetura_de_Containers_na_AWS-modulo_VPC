3d17954bfe6d92923ed4b4392f94a0a549d481d4
Luiz-H3nrique <luizsantos894@gmail.ccom>
2024-08-25 21:06:22 -0300

    featurea: criação de subnets,vpc,Nat Gateway,parameters store

---
+++ variables.tf
@@ -0,0 +1,9 @@
+##### General CONFIGS #####
+
+variable "project_name" {
+  description = "nome do recurso a ser criado"
+}
+
+variable "region" {
+  description = "região onde sera criada o recurso"
+}
